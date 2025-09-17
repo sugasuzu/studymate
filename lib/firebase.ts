c3cd695e087879616148112ede8e11d4a9aae021
huteneko-programing <huteneko2024@gmail.com>
2025-09-17 21:36:50 +0900

    firebaseの環境変数を設定

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnL-JHYv-PCmPspN9sef9j6vF3ovSu6pc",
-  authDomain: "studymate-4ba45.firebaseapp.com",
-  projectId: "studymate-4ba45",
-  storageBucket: "studymate-4ba45.firebasestorage.app",
-  messagingSenderId: "537351683696",
-  appId: "1:537351683696:web:9a974ba473f476cc163eb5",
-  measurementId: "G-DQDBFKT0BL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only if it hasn't been initialized
